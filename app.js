7697a23f53575b41a4d56f0cf46f85581bf3db62
cobalt88 <vincentteune@gmail.com>
2022-04-14 13:57:47 -0400

    4.14.22
    module 9.2.6

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs')
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-
-/*
 const printProfileData = profileDataArr => {
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
-*/
 
+
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
