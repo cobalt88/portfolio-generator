9e160145b57331a7d61393212beb3ad04a0407f4
cobalt88 <vincentteune@gmail.com>
2022-04-13 23:11:28 -0400

    4.13.22
    end of day

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+
+/*
 const printProfileData = profileDataArr => {
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
+*/
+
